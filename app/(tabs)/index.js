51b0a4c932174422b2863f030f66d7ade79fc7c3
hailegithubaccount <michaelhaile1222@gmail.com>
2025-07-26 00:28:28 -0700

    a

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,34 +1,86 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router'; 
+import { useRouter } from 'expo-router';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const CalendarIcon = () => {
+const AddMoney = () => {
   const router = useRouter();
 
   return (
-    <TouchableOpacity 
-      onPress={() => router.push('/openPage')}
-      style={styles.container}
-    >
-      <View style={styles.centerContainer}>
-        <Text style={styles.TheText}>REVOLUT</Text>
+    <View style={styles.Main}>
+      <View style={styles.icons}>
+      <TouchableOpacity 
+            onPress={() => router.push('/openPage')}
+            style={styles.container}
+          >
+            <View style={styles.iconarrow}>
+            <Icon name="arrow-left" size={20} color="white" />
+
+            </View>
+          </TouchableOpacity>
+      
+         <TouchableOpacity 
+            onPress={() => router.push('/openPage')}
+            style={styles.container}
+          >
+            <View style={styles.iconarrow}>
+           <Icon name="shield" size={30} color="white" />
+            </View>
+          </TouchableOpacity>
+      <View style={styles.icons}></View>
+       
+          
+        
       </View>
-    </TouchableOpacity>
+      <View style={{justifyContent:'flex-start'}}>
+        <Text style={{color:'white',fontSize:35,fontWeight:'bold'}}>
+          Add Money
+        </Text>
+      </View>
+      <View style={styles.box}>
+         <Icon name="arrow-left" size={20} color="white" />
+         <Text style={{color:'white'}}>hailemichael</Text>
+         
+            <TouchableOpacity 
+                  style={styles.button} 
+                 
+                >
+                  <Text style={styles.buttonText}>Account</Text>
+                </TouchableOpacity>
+
+    
+
+
+         
+
+
+
+
+       
+
+      </View>
+
+
+
+    </View>
+  
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-  },
-  centerContainer: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-  },
-  TheText: {
-    fontSize: 40,
-    fontWeight: 'bold',
-  },
+ Main:{
+  flex:1,
+  backgroundColor:"black"
+ },
+ icons:{
+  flexDirection:'row',
+  gap:260,
+  marginHorizontal:20,
+  marginTop:20
+  
+
+ },
+
+
 });
 
-export default CalendarIcon;
+export default AddMoney;
