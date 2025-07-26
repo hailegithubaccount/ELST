e25d61f36da9ebb2200f2e8896cfab61e97b3a6c
hailegithubaccount <michaelhaile1222@gmail.com>
2025-07-25 18:18:16 -0700

    adding the icons

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,10 +1,10 @@
-import { View, TextInput, StyleSheet, SafeAreaView, Image, ImageBackground } from 'react-native';
+import { View, TextInput, StyleSheet, SafeAreaView, Image, ImageBackground,Text,TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const SearchScreen = () => {
   return (
     <ImageBackground
-      source={require('../../assets/images/e.png')} 
+      source={require('../../assets/images/s.png')} 
       style={styles.backgroundImage}
       resizeMode="cover"
     >
@@ -36,12 +36,121 @@ const SearchScreen = () => {
   </View>
           </View>
         </View>
+        <View style={styles.text}>
+          <View style={styles.firstandsecondtext}>
+            <Text style={styles.text1}>Personal.Account</Text>
+            <Text style={styles.text2}>$12.
+               <Text style={styles.smallText}>54</Text>
+            </Text>
+           <TouchableOpacity 
+        style={styles.button} 
+       
+      >
+        <Text style={styles.buttonText}>Account</Text>
+      </TouchableOpacity>
+
+          </View>
+           
+
+
+
+
+
+
+        </View>
+{/* the following one is the four middle tab */}
+
+        <View style={styles.MiddleTab}>
+
+          <View style={styles.withtext}>
+             <TouchableOpacity 
+        style={styles.Tabbutton} 
+       
+      >
+         <Icon name="plus" size={20} color="white" style={styles.iconTab} />
+      </TouchableOpacity>
+       <Text style={styles.text1}>Personal</Text>
+      
+
+          </View>
+
+
+
+
+
+            <View style={styles.withtext}>
+             <TouchableOpacity 
+        style={styles.Tabbutton} 
+       
+      >
+         <Icon name="exchange" size={20} color="white" style={styles.iconTab} />
+      </TouchableOpacity>
+       <Text style={styles.text1}>Personal</Text>
+      
+
+          </View>
+            <View style={styles.withtext}>
+             <TouchableOpacity 
+        style={styles.Tabbutton} 
+       
+      >
+         <Icon name="home" size={20} color="white" style={styles.iconTab} />
+      </TouchableOpacity>
+       <Text style={styles.text1}>Personal</Text>
+      
+
+          </View>
+            <View style={styles.withtext}>
+             <TouchableOpacity 
+        style={styles.Tabbutton} 
+       
+      >
+       <Icon name="ellipsis-v" size={20} color="white" style={styles.iconTab} />
+
+      </TouchableOpacity>
+       <Text style={styles.text1}>Personal</Text>
+      
+
+          </View>
+          
+</View>
+
+
+
+
+       
       </SafeAreaView>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+ 
+
+  MiddleTab:{
+    flexDirection:'row',
+     justifyContent:'space-between',
+     marginBottom:70,
+     marginEnd:10,
+     marginStart:10,
+    
+     
+
+  },
+  Tabbutton:{
+    backgroundColor: "#696969",
+    alignItems:'center',
+    color:'white',
+   
+    alignContent:'center',
+     justifyContent:'center', 
+
+alignItems:'center'    ,
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+
+  },
   backgroundImage: {
     flex: 1,
     width: '100%',
@@ -100,6 +209,45 @@ const styles = StyleSheet.create({
   icon: {
     marginHorizontal: 8,
   },
+  firstandsecondtext:{
+   
+    alignItems:'center',
+    justifyContent:'center',
+    marginTop:50,
+
+    
+  },
+  text1:{
+    color:'white',
+    fontSize:15,
+
+  },
+  text2:{
+    color:'white',
+    fontSize:50,
+    
+
+  },
+  smallText:{
+     fontSize:25,
+
+  },
+    button: {
+    backgroundColor: "#696969",
+    padding: 10,
+    borderRadius:50,
+    minWidth: 100,
+    alignItems: 'center',
+     marginBottom:100,
+  },
+  buttonText: {
+    color: '#D3D3D3',
+    fontSize: 18,
+    fontWeight: 'bold',
+   
+  },
+
+
 });
 
 export default SearchScreen;
