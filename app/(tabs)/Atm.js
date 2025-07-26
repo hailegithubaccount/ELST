f2fa194b8b1cc6b2e9f34ddcf332731cd3e2b6b5
hailegithubaccount <michaelhaile1222@gmail.com>
2025-07-26 04:45:49 -0700

    ad

---
+++ app/(tabs)/Atm.js
@@ -0,0 +1,35 @@
+
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+
+const CalendarIcon = () => {
+  const router = useRouter();
+
+  return (
+    <TouchableOpacity
+      onPress={() => router.push('/openPage')}
+      style={styles.container}
+    >
+      <View style={styles.centerContainer}>
+        <Text style={styles.TheText}>REVOLUT</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  TheText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+});
+
+export default CalendarIcon;
